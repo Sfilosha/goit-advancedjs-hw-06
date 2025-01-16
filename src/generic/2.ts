0c37bfb952f5a6e23f3d8c30ab5341756a0d514e
Sfilosha <sfilosha@gmail.com>
2025-01-16 21:01:52 +0200

    feedback fix

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,10 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare(
-  top:Pick<AllType, 'name' | 'color'>,
-  bottom: Pick<AllType, 'position' | 'weight'>
-): AllType {
+function compare<objectData extends AllType>(top:objectData, bottom: objectData): AllType {
   return {
     name: top.name,
     color: top.color,
